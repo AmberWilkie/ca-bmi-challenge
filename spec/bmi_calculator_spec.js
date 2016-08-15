96b0674dbd9633fd06433b99f5c36d45d8d22c84
Amber <amber@amberwilkie.com>
2016-08-15 20:12:09 +0200

    First commit, waaaaay too late. Logic tests are passing fine. UI tests are passing, after considerable work. Javascript is a pain.

---
+++ spec/bmi_calculator_spec.js
@@ -0,0 +1,27 @@
+describe("BMICalculator", function() {
+  var calculator, person;
+
+  beforeEach(function() {
+    person = new Person({weight: 90, height: 186});
+    calculator = new BMICalculator();
+  });
+
+  it("calculates BMI for a person using metric method", function() {
+    calculator.metric_bmi(person);
+    expect(person.bmiValue).toEqual(26.01);
+  });
+
+});
+
+describe("BMICalculator", function(){
+  beforeEach(function() {
+    person = new Person({weight: 160, height_feet: 5, height_inches: 6});
+    calculator = new BMICalculator();
+  });
+
+  it("calculates BMI for a person using imperial method", function() {
+    calculator.imperial_bmi(person);
+    expect(person.bmiValue).toEqual(25.82);
+  });
+
+});
---
+++ spec/person_spec.js
@@ -0,0 +1,40 @@
+
+describe("Person", function() {
+  var person;
+
+  beforeEach(function() {
+    person = new Person({weight: 90, height: 186, system: "metric"});
+  });
+
+  it("should have weight of 90", function() {
+    expect(person.weight).toEqual(90);
+  });
+
+  it("should have height of 186", function() {
+    expect(person.height).toEqual(186);
+  });
+
+  it("should calculate BMI value", function() {
+    person.calculate_bmi();
+    expect(person.bmiValue).toEqual(26.01)
+  });
+
+  it("should have a BMI Message", function() {
+    person.calculate_bmi();
+    expect(person.bmiMessage).toEqual("need a few more hamburgers...")
+  });
+});
+
+describe("Imperial Person", function() {
+  var person;
+
+  beforeEach(function() {
+    person = new Person({weight: 160, height_feet: 5, height_inches: 6, system: "imperial"});
+  });
+
+  it("should have a BMI message", function() {
+    person.calculate_bmi();
+    expect(person.bmiMessage).toEqual("need a few more hamburgers...")
+  });
+
+});
